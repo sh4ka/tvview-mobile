31e0c197bb28606bc60e0e2339aaa9bd0786b1ac
Jesus Flores <apunta666@gmail.com>
2011-12-08 22:10:06 +0100

    Lungojs added and list of shows for today created.

--- index.php
+++ index.php
@@ -17,8 +17,43 @@ foreach ($yearShowsData as $day) {
 	{
 		// we found today shows
 		 $todayData = $day;
-		 var_dump($todayData);
+		 //var_dump($todayData);
 	}
-    //printf("%s tiene %d hijos.\n", $day['attr'], $day->count());
 }
 ?>
+<!doctype html>
+<html manifest="index.appcache">
+<?php
+	include_once('includes/includes.php');
+?>
+</head> 
+
+<body class="app">
+    <!--
+        First, you have to do is create a LungoJS Application instance in the file app.js.
+        ...and use a Webkit browser as Chrome or Safari.
+    -->
+    <section id="section_tvview">
+        <header data-title="tvview-mobile"></header>                
+        <article id="articulo1" class="list">
+        	<?php
+        		foreach ($todayData as $time) {
+        			$timeStep = $time;		
+					foreach ($timeStep as $show) {
+						echo '<li><strong>'.$show['name'].'</strong><small>'.$timeStep['attr'].'</small></li>';
+					}					
+				}
+        	?>        	
+        </article>        
+    </section>   
+
+    <!-- LungoJS (Production mode) -->
+    <script src="lungo.js/lungo-1.0.2.packed.js"></script>
+    <!-- LungoJS - Sandbox App -->    
+    <script src="app/app.js"></script>
+    <script src="app/data.js"></script>
+    <script src="app/events.js"></script>
+    <script src="app/services.js"></script>
+    <script src="app/view.js"></script>
+</body> 
+</html>
